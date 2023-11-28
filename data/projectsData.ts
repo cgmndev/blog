83cafcc2d7010832ba5599c9783735842b78ad27
Claudio Mardones <claudio.mnec@gmail.com>
2023-11-28 20:25:10 -0300

    Fix array projects. Add sample project.

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,12 +1,10 @@
 const projectsData = [
-  // {
-  //   title: 'A Search Engine',
-  //   description: `What if you could look up any information in the world? Webpages, images, videos
-  //   and more. Google has many features to help you find exactly what you're looking
-  //   for.`,
-  //   imgSrc: '/static/images/google.png',
-  //   href: 'https://www.google.com',
-  // },
+  {
+    title: 'cgmn.dev blog',
+    description: `Blog desarrollado con tecnología Next.js desplegado en Vercel conectado con Github.`,
+    imgSrc: '/static/images/cgmn_logo.png',
+    href: 'https://cgmn.dev',
+  },
   // {
   //   title: 'The Time Machine',
   //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
